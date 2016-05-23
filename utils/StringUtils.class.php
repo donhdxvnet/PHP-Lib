24b62d84aeee1d3dc8fc81b189ba733cae743fff
Software 0044 <software0044@users.noreply.github.com>
2016-05-23 11:06:18 +0200

    Method "isAnagram()"
    Detect if a word is an anagram of another

--- utils/StringUtils.class.php
+++ utils/StringUtils.class.php
@@ -2,6 +2,26 @@
 
 abstract class StringUtils
 {	
+	public static function isAnagram()
+	{
+		$wrd_1 = strtolower($wrd_1);
+		$wrd_2 = strtolower($wrd_2);
+		for($iter=0; $iter<strlen($wrd_1); $iter++) {
+			$char = substr($wrd_1, $iter, 1);
+			if (substr_count($wrd_1,$char)!=substr_count($wrd_2,$char)) {
+				return false;
+			}
+		}
+		for($iter=0; $iter<strlen($wrd_2); $iter++) {
+			$char = substr($wrd_2, $iter, 1);
+			if (substr_count($wrd_1, $char) != substr_count($wrd_2, $char)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
 	/**
 	 * Function: setData
 	 * Filtrer un texte saisi
