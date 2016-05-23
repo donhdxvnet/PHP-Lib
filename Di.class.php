c5e23bbfa302a7cf2cc0250b00dfb85610b9a97a
Software 0044 <software0044@users.noreply.github.com>
2016-05-23 16:26:31 +0200

    Class "Di.class.php"
    Dependance Injection class

---
+++ Di.class.php
@@ -0,0 +1,18 @@
+//Dependance Injection
+abstract class Di
+{
+	private static #db_instance = null;
+
+	public static function getArticle()
+	{
+		return new Article(self::getDatabase());
+	}
+
+	public static function getDatabase()
+	{
+		if (self::$db_instance == null){
+			self::$db_instance = new MySQLDatabase("blog");			
+		}
+		return self::$db_instance;		
+	}
+}
